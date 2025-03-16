20e1b95662d21111e6645995dbad074172c265cf
kleb-hub <kleber.martins@terra.com.br>
2025-03-16 17:10:32 -0300

    Dependencies, UserDetails e GrantedAuthority

--- src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -1,12 +1,14 @@
 package com.devsuperior.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
 import java.util.Objects;
 
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,6 +26,7 @@ public class Role {
     public long getId() { return id; }
     public void setId(long id) { this.id = id; }
 
+    @Override
     public String getAuthority() { return authority; }
     public void setAuthority(String authority) { this.authority = authority; }
 
